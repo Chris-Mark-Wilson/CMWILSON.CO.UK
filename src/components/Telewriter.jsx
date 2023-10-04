996456945044761b0d4ea09966579eef300df301
bigsegs <cw.plastering.services@gmail.com>
2023-10-04 14:37:10 +0100

    built for deploy

--- src/components/Telewriter.jsx
+++ src/components/Telewriter.jsx
@@ -6,78 +6,87 @@ export const Telewriter = ({ txt, startPos }) => {
   const [feed, setFeed] = useState([]);
   const [timer, setTimer] = useState(0);
   const [index, setIndex] = useState(0);
-  const {written, setWritten} = useContext(WrittenContext);
-const[block,setBlock]=useState(0)
+  const { written, setWritten } = useContext(WrittenContext);
+  const [block, setBlock] = useState(0);
   const [para, setPara] = useState(0);
+  const [txtArray, setTxtArray] = useState(txt[0].split("\n"));
+  const[end,setEnd]=useState(false)
   const screenRef = useRef(null);
-const[txtArray,setTxtArray]=useState(txt[0].split("\n"))
 
-
-  
   useEffect(() => {
- 
     setFeed(() => {
       return new Array(txtArray.length).fill("\r");
     });
- 
-    screenRef.current.focus()
+
+    screenRef.current.focus();
   }, [txtArray]);
 
-
-
+  ///////////////TIMER//////////////
   useEffect(() => {
     setTimeout(() => {
-        timer===1000&&setTimer(()=>0)
+      timer === 1000 && setTimer(() => 0);
       setTimer(timer + 1);
     }, timerInterval);
   }, [timer]);
 
+  //////////TELEWRITER////////////
   useEffect(() => {
-      if(!written){
-
-    if (index=== txtArray[para].length-1) {
-      
-      setPara(para=>para+1);
-      setIndex(()=>0)
-    }
-    if(txtArray[para][index]==="k"){
+    if (!written) {
+         
+      if (index === txtArray[para].length - 1) {
+        setPara((para) => para + 1);
+        setIndex(() => 0);
+      }
+      if (txtArray[para][index] === "k") {
+        setWritten(true);
+      }
+      if (txtArray[para][index] === "e") {
         setWritten(true)
-    }
-    setFeed((oldFeed) => {
-      
-      oldFeed[para]+=(txtArray[para][index]);
-      setIndex((index)=>index+1)
-      return [...oldFeed];
-    });
+        setEnd(true);
+      }
+      setFeed((oldFeed) => {
+        oldFeed[para] += txtArray[para][index];
+        setIndex((index) => index + 1);
+        return [...oldFeed];
+      });
 
-    //scrollHeight increases beyond  clientHeight on overflow so...
-    screenRef.current.scrollTo({
-      top: screenRef.current.scrollHeight,
-      left: 0,
-      behaviour: "smooth",
-    });
-}
+      //scrollHeight increases beyond  clientHeight on overflow so...
+      screenRef.current.scrollTo({
+        top: screenRef.current.scrollHeight,
+        left: 0,
+        behaviour: "smooth",
+      });
+    }
   }, [timer]);
-  
-const handleKeyDown=(e)=>{
-    if(written){
-     if(txt.length-1>block){
-        setBlock((block)=>block+1)
-        setTxtArray(()=> txt[block].split("\n"))
-        setWritten(false)
+  useEffect(() => {
+    setTxtArray(() => txt[block].split("\n"));
+  }, [block]);
 
-        
-
-     }
+  const handleKeyDown = (e) => {
+    console.log(written)
+    if (written) {
+      if (txt.length - 1 > block) {
+        setBlock(block + 1);
+        setPara(0);
+        setIndex(0);
+        setWritten(false);
+      }
     }
+    if(written&&end){
+        console.dir(window.location)
+switch (e.nativeEvent.key.toLowerCase()){
+    case "1": window.location.replace('https://chriswilsonncnews.netlify.app/');
+    break;
+    case"2":window.location.replace('https://github.com/Chris-Mark-Wilson/SafeJourney')
+    break;
+    case"3":window.location.replace('https://sabotage81.onrender.com')
+    default:break;
 }
-
-
-
-
+    }
+  };
 
   return (
-    <div id="screen" ref={screenRef} tabIndex={0}onKeyDown={handleKeyDown} >
+    <div id="screen" ref={screenRef} tabIndex={0} onKeyDown={handleKeyDown}>
       {feed.map((paragraph, index) => {
         return (
           <p
@@ -88,9 +97,10 @@ const handleKeyDown=(e)=>{
               top: `${startPos.top}`,
               left: `${startPos.left}`,
             }}
-            onClick={()=>{
-                console.log("clicked")
-                screenRef.current.focus()}}
+            onClick={() => {
+              console.log("clicked");
+              screenRef.current.focus();
+            }}
           >
             {paragraph}
           </p>
